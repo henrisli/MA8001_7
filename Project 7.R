53a5354109f356164c6dc09a5303285f6f849c2d
henrisli <henrisli@stud.ntnu.no>
2019-11-04 16:55:06 +0100

    ferdig?

--- Project 7.R
+++ Project 7.R
@@ -64,17 +64,105 @@ for (j in 1:25){
   R_j[[j]] = covariance_mat%*%t(F_j)%*%solve(F_j%*%covariance_mat%*%t(F_j)+obs_cov_mat)%*%F_j%*%covariance_mat
   r_wj2 = sum(R_j[[j]])
   r_wj[j] = sqrt(r_wj2)
-  observed_indexes = seq(j, 600 + j, l = 25)
-  cov_mat_temp = covariance_mat[observed_indexes, observed_indexes]
-  voi = 0
+  voi_j[j] = r_wj[j]*dnorm(0)
+  #observed_indexes = seq(j, 600 + j, l = 25)
+  #cov_mat_temp = covariance_mat[observed_indexes, observed_indexes]
+  #voi = 0
   
-  y = mvrnorm(200000, mu = rep(0,25), Sigma = cov_mat_temp) + matrix(rnorm(25*200000, 0, 5), ncol = 25)
-  mu_j = covariance_mat%*%t(F_j)%*%solve(F_j%*%covariance_mat%*%t(F_j)+obs_cov_mat)%*%t(y)
-  mu_w = apply(mu_j, 2, sum)
-  voi = mu_w*pnorm(mu_w/r_wj[j]) + r_wj[j]*dnorm(mu_w/r_wj[j])
-  voi_j[j] = mean(voi)
+  # y = mvrnorm(200000, mu = rep(0,25), Sigma = cov_mat_temp) + matrix(rnorm(25*200000, 0, 5), ncol = 25)
+  # mu_j = covariance_mat%*%t(F_j)%*%solve(F_j%*%covariance_mat%*%t(F_j)+obs_cov_mat)%*%t(y)
+  # mu_w = apply(mu_j, 2, sum)
+  # voi = mu_w*pnorm(mu_w/r_wj[j]) + r_wj[j]*dnorm(mu_w/r_wj[j])
+  # voi_j[j] = mean(voi)
   print(j)
 }
 plot(voi_j, xlab = "j", ylab = "VoI")
 
 # b) second part
+simulation_run <- function(){
+  covariance_mat = correlation_mat
+  x = mvrnorm(1, mu = rep(0,625), Sigma = covariance_mat)
+  r_wj = rep(NA, 25)
+  voi_j = rep(NA, 25)
+  for (j in 1:25){
+    F_j = matrix(0, ncol = 625, nrow = 25)
+    for (i in 1:25){
+      F_j[i,j+25*(i-1)] = 1
+    }
+    R_j[[j]] = covariance_mat%*%t(F_j)%*%solve(F_j%*%covariance_mat%*%t(F_j)+obs_cov_mat)%*%F_j%*%covariance_mat
+    r_wj2 = sum(R_j[[j]])
+    r_wj[j] = sqrt(r_wj2)
+    voi_j[j] = r_wj[j]*dnorm(0)
+  }
+  survey_line = list()
+  survey_line[[1]] = which.max(voi_j)
+  r_wj = r_wj[survey_line[[1]]]
+  F_j = matrix(0, ncol = 625, nrow = 25)
+  for (i in 1:25){
+    F_j[i,survey_line[[1]]+25*(i-1)] = 1
+  }
+  y = F_j%*%x + rnorm(25, 0, 5)
+  mu_new = covariance_mat%*%t(F_j)%*%solve(obs_cov_mat + F_j%*%covariance_mat%*%t(F_j))%*%y
+  covariance_mat = covariance_mat - R_j[[survey_line[[1]]]]
+  mu_w = sum(mu_new)
+  iter = 1
+  while(max(0,mu_w) < mu_w*pnorm(mu_w/r_wj) + r_wj*pnorm(mu_w/r_wj) - 0.5){
+    iter = iter+1
+    r_wj = rep(NA, 25)
+    voi_j = rep(NA, 25)
+    for (j in 1:25){
+      F_j = matrix(0, ncol = 625, nrow = 25)
+      for (i in 1:25){
+        F_j[i,j+25*(i-1)] = 1
+      }
+      R_j[[j]] = covariance_mat%*%t(F_j)%*%solve(F_j%*%covariance_mat%*%t(F_j)+obs_cov_mat)%*%F_j%*%covariance_mat
+      r_wj2 = sum(R_j[[j]])
+      r_wj[j] = sqrt(r_wj2)
+      voi_j[j] = r_wj[j]*dnorm(0)
+    }
+    survey_line[[iter]] = which.max(voi_j)
+    r_wj = r_wj[survey_line[[iter]]]
+    F_j = matrix(0, ncol = 625, nrow = 25)
+    for (i in 1:25){
+      F_j[i,survey_line[[iter]]+25*(i-1)] = 1
+    }
+    y = F_j%*%x + rnorm(25, 0, 5)
+    mu_new = covariance_mat%*%t(F_j)%*%solve(obs_cov_mat + F_j%*%covariance_mat%*%t(F_j))%*%y
+    covariance_mat = covariance_mat - R_j[[survey_line[[iter]]]]
+    mu_w = sum(mu_new)
+    #print(mu_w)
+    #print(iter)
+  }
+  return(unlist(survey_line))
+}
+survey_lines = list()
+for (i in 1:10){
+  print(i)
+  survey_lines[[i]] = simulation_run()
+}
+#write.table(survey_lines, "C://Users//henri//Documents//GitHub//MA8001_7//survey_lines.csv")
+num_iter = sapply(survey_lines, length)
+df = data.frame(x = num_iter)
+ggplot(data = df, aes(x=x)) + geom_histogram() + xlab("Number of survey lines") + ylab("Counts")
+#hist(num_iter)
+
+
+
+max_iterations = max(num_iter)
+
+line_number = rep(NA, sum(num_iter))
+run_number = rep(NA, sum(num_iter))
+plot_matrix = matrix(0, nrow = 100, ncol = 25)
+iter = 1
+for(i in 1:204){
+  for (j in 1:num_iter[i]){
+    line_number[iter] = survey_lines[[i]][j]
+    run_number[iter] = i
+    plot_matrix[i, survey_lines[[i]][j]] = plot_matrix[i, survey_lines[[i]][j]] + 1
+    iter = iter + 1
+  }
+}
+
+plot(run_number, line_number, xlab = "Run number", ylab = "Survey Line")
+image.plot(matrix(apply(plot_matrix, 2, sum), ncol = 1), yaxt = 'n', x = 1:25, y = 1, ylab = "", xlab = "Survey Line")
+plot(apply(plot_matrix, 2, sum), xlab = "Survey Line", ylab = "Number of surveys")
